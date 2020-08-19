7a03f7516589557c452b50fab7d60b44b2aec1b4
a-kisiel <60425431+a-kisiel@users.noreply.github.com>
2020-08-19 08:39:12 -0400

    Added Color Wheel
    - Added a fancy new color wheel (credit given in the html)
    - Added local storage 'cookie' to save color preferences
    - Minor formatting edits to all files in the interest of tidiness

--- localhome.js
+++ localhome.js
@@ -1,45 +1,24 @@
-// Local 'Cookie' Operations
-
-// Bakes a fresh new cookie
-function bakeCookie(cooKey, value) {
-    localStorage.setItem(cooKey, value)
-}
-
-// Checks whether a given cookie exists
-function isCookie(cooKey) {
-    return (localStorage.getItem(cooKey) != null)
-}
-
-// Gets a value from an existing cookie using a key
-function getCookie(cooKey) {
-    return localStorage.getItem(cooKey)
-}
-
-// Default preferences
-var is24 = false;
-var voiceOn = true;
-
-// Alters preferences if a cookie exists
-is24 = (getCookie('ac') === 'active') ? true : false;
-voiceOn = (getCookie('vc') === 'active') ? true : false;
-
-console.log("AC Cookie: " + getCookie('ac'))
-console.log("VC Cookie: " + getCookie('vc'))
+// Alters preferences if a local 'cookie' exists
+var is24 = (localStorage.getItem('ac') === 'active') ? true : false;
+var voiceOn = (localStorage.getItem('vc') === 'active') ? true : false;
+var storedColor = (localStorage.getItem('color') == "") ? '#17182f' : localStorage.getItem('color');
 
 document.getElementById('vcSwitch').checked = voiceOn ? true : false
 document.getElementById('acSwitch').checked = is24 ? true : false
-
-// Preferences adjustments
-
-function toggleVC() {
-  voiceOn = voiceOn ? false : true;
-  let newVC = voiceOn ? 'active' : 'inactive';
-  bakeCookie('vc', newVC);
-  let newAC = is24 ? 'active' : 'inactive';
-  bakeCookie('ac', newAC);
-}
+document.getElementById('enterColor').placeholder = storedColor;
+document.body.style.backgroundColor = storedColor;
 
 var today = new Date();
+
+// Color wheel initialization
+var colorPicker = new iro.ColorPicker('#picker', {
+  width: 220,
+  color: storedColor
+});
+
+colorPicker.on("color:change", () => {
+  changeColor(colorPicker.color.hexString);
+})
 
 // Normal time
 function normalTime() {
@@ -48,15 +27,18 @@ function normalTime() {
   let qual;
 
   if (h < 13)
-      qual = 'am';
+    qual = 'am';
   else {
-      h -= 12;
-      qual = 'pm';
+    h -= 12;
+    qual = 'pm';
+  }
+  if (h == 0) {
+    h = 12;
   }
   if (m < 10)
-      m = '0' + m;
+    m = '0' + m;
 
-    document.getElementById('stardate').innerHTML = h + ":" + m + " " + qual;
+  document.getElementById('stardate').innerHTML = h + ":" + m + " " + qual;
 }
 
 // 24-hour time
@@ -74,10 +56,12 @@ var tellTime = is24 ? time24 : normalTime;
 
 tellTime();
 
+// Alter preferences
+
 function toggleClock() {
   if (is24) {
     is24 = false;
-    tellTime = normalTime; 
+    tellTime = normalTime;
   }
   else {
     is24 = true;
@@ -85,13 +69,40 @@ function toggleClock() {
   }
 
   let newAC = (is24 == true) ? 'active' : 'inactive';
-  bakeCookie('ac', newAC);
-  let newVC = (voiceOn == true) ? 'active' : 'inactive';
-  bakeCookie('vc', newVC);
+  localStorage.setItem('ac', newAC);
 }
 
+function toggleVC() {
+  voiceOn = voiceOn ? false : true;
+  let newVC = voiceOn ? 'active' : 'inactive';
+  localStorage.setItem('vc', newVC);
+}
+
+function changeColor(color) {
+  if (!color.startsWith('#'))
+    color = '#' + color;
+  // Verifies hex code as acceptable for color code
+  if (!(/^#(?:[0-9a-fA-F]{3}){1,2}$/.test(color))) {
+    document.getElementById('colorNote').innerHTML = "Enter a valid hex code";
+    document.getElementById('colorNote').style.display = 'block';
+    return;
+  }
+  document.getElementById('colorNote').innerHTML = "Original color was 17182f";
+  if (document.body.style.backgroundColor != '#17182f')
+    document.getElementById('colorNote').style.display = 'block';
+  else
+    document.getElementById('colorNote').style.display = 'none';
+
+  localStorage.setItem('color', color);
+  document.getElementById('enterColor').value = color;
+  document.body.style.backgroundColor = color;
+  colorPicker.color.hexString = color;
+}
+
+/* Utility methods */
+
 // Writes time to DOM every second
-setInterval(function() {
+setInterval(function () {
   tellTime();
 }, 1000)
 
@@ -102,18 +113,23 @@ document.getElementById('warp9').innerHTML = prompts[Math.floor(Math.random() * 
 
 // Check whether String is a url
 function validURL(str) {
-  var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
-    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
-    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
-    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
-    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
-    '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+  var pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+    '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+    '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
   return !!pattern.test(str);
+}
+
+// Reverts to the first color passed to the window
+function revertColor() {
+  colorPicker.reset();
 }
 
 // Intercepts 'enter' keypress
 var coord = document.getElementById('heading');
-coord.addEventListener("keyup", function(event) {
+coord.addEventListener("keyup", function (event) {
   if (event.keyCode === 13) {
     event.preventDefault();
     document.getElementById('warp9').click();
@@ -129,18 +145,18 @@ function engage() {
     return;
 
   if (voiceOn) {
-      let engageNo = Math.floor(Math.random() * 7)
-      var picard = new Audio("Engage mp3s/engage" + engageNo + ".mp3");
-      picard.play();
-      if (validURL(heading))
-        window.open('http://' + heading);
-      else  
-        window.open('http://google.com/search?q=' + heading);
+    let engageNo = Math.floor(Math.random() * 7)
+    var picard = new Audio("Engage mp3s/engage" + engageNo + ".mp3");
+    picard.play();
+    if (validURL(heading))
+      window.open('http://' + heading);
+    else
+      window.open('http://google.com/search?q=' + heading);
 
-      setTimeout(() => {
-        window.close()
-      }, 2400);
-    }
+    setTimeout(() => {
+      window.close()
+    }, 2400);
+  }
   else {
     window.open('http://google.com/search?q=' + heading, '_self');
   }
@@ -148,27 +164,26 @@ function engage() {
 
 // Open/close the preferences menu
 function togglePrefs() {
-  if (document.getElementById('prefpanel').style.width != "260px") {
-    document.getElementById('prefpanel').style.width = "260px";
+  if (document.getElementById('sidebar').style.marginLeft != '0px') {
+    document.getElementById('sidebar').style.marginLeft = '0px';
     let elements = document.getElementsByClassName('switch');
-    for (var i=0; i<elements.length; i++) {
+    for (var i = 0; i < elements.length; i++) {
       elements[i].style.display = 'block';
     }
-    setTimeout(function(){
-      elements = document.getElementsByClassName('preflabel');
-      for (var i=0; i<elements.length; i++) {
+    document.getElementById('colorNote').style.display = 'none';
+    elements = document.getElementsByClassName('preflabel');
+    for (var i = 0; i < elements.length; i++) {
       elements[i].style.display = 'block';
-      }
-    }, 400)
+    }
   }
   else {
-    document.getElementById('prefpanel').style.width = "0px";
+    document.getElementById('sidebar').style.marginLeft = '-260px';
     let elements = document.getElementsByClassName('switch');
-    for (var i=0; i<elements.length; i++) {
+    for (var i = 0; i < elements.length; i++) {
       elements[i].style.display = 'none';
     }
     elements = document.getElementsByClassName('preflabel');
-    for (var i=0; i<elements.length; i++) {
+    for (var i = 0; i < elements.length; i++) {
       elements[i].style.display = 'none';
     }
   }
@@ -177,112 +192,112 @@ function togglePrefs() {
 /* Begin particle implementation */
 
 particlesJS("particles-js", {
-    "particles": {
-      "number": {
-        "value": 355,
-        "density": {
-          "enable": true,
-          "value_area": 789.1476416322727
-        }
+  "particles": {
+    "number": {
+      "value": 355,
+      "density": {
+        "enable": true,
+        "value_area": 789.1476416322727
+      }
+    },
+    "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
       },
-      "color": {
-        "value": "#ffffff"
+      "polygon": {
+        "nb_sides": 5
       },
-      "shape": {
-        "type": "circle",
-        "stroke": {
-          "width": 0,
-          "color": "#000000"
-        },
-        "polygon": {
-          "nb_sides": 5
-        },
-        "image": {
-          "src": "img/github.svg",
-          "width": 100,
-          "height": 100
-        }
-      },
-      "opacity": {
-        "value": 0.48927153781200905,
-        "random": false,
-        "anim": {
-          "enable": true,
-          "speed": 0.2,
-          "opacity_min": 0,
-          "sync": false
-        }
-      },
-      "size": {
-        "value": 2,
-        "random": true,
-        "anim": {
-          "enable": true,
-          "speed": 2,
-          "size_min": 0,
-          "sync": false
-        }
-      },
-      "line_linked": {
-        "enable": false,
-        "distance": 150,
-        "color": "#ffffff",
-        "opacity": 0.4,
-        "width": 1
-      },
-      "move": {
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 0.48927153781200905,
+      "random": false,
+      "anim": {
         "enable": true,
         "speed": 0.2,
-        "direction": "none",
-        "random": true,
-        "straight": false,
-        "out_mode": "out",
-        "bounce": false,
-        "attract": {
-          "enable": false,
-          "rotateX": 600,
-          "rotateY": 1200
-        }
+        "opacity_min": 0,
+        "sync": false
       }
     },
-    "interactivity": {
-      "detect_on": "canvas",
-      "events": {
-        "onhover": {
-          "enable": true,
-          "mode": "bubble"
-        },
-        "onclick": {
-          "enable": true,
-          "mode": "push"
-        },
-        "resize": true
+    "size": {
+      "value": 2,
+      "random": true,
+      "anim": {
+        "enable": true,
+        "speed": 2,
+        "size_min": 0,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": false,
+      "distance": 150,
+      "color": "#ffffff",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 0.2,
+      "direction": "none",
+      "random": true,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "bubble"
       },
-      "modes": {
-        "grab": {
-          "distance": 400,
-          "line_linked": {
-            "opacity": 1
-          }
-        },
-        "bubble": {
-          "distance": 83.91608391608392,
-          "size": 1,
-          "duration": 3,
-          "opacity": 1,
-          "speed": 3
-        },
-        "repulse": {
-          "distance": 200,
-          "duration": 0.4
-        },
-        "push": {
-          "particles_nb": 4
-        },
-        "remove": {
-          "particles_nb": 2
-        }
-      }
+      "onclick": {
+        "enable": true,
+        "mode": "push"
+      },
+      "resize": true
     },
-    "retina_detect": true
-  });
+    "modes": {
+      "grab": {
+        "distance": 400,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 83.91608391608392,
+        "size": 1,
+        "duration": 3,
+        "opacity": 1,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 200,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
+});
